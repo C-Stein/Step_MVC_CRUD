fa897db4e725283cbdabe0799cc7b8cc2aa4866b
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-03 13:09:10 -0500

    added models

---
+++ Step_MVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Step_MVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public String Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
