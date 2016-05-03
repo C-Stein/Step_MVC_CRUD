dd36b4d2d3b576d6b6d24643a3f73a1abf187051
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-03 13:27:05 -0500

    added and configured a controller and initial view

---
+++ Step_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Step_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Step_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            DataStoreContext _myEmployeeContext = new DataStoreContext();
+            var query = from c in _myEmployeeContext.Employee
+
+                        select c;
+
+            return View(query.ToList());
+        }
+    }
+}
